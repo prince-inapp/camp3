5e31b679954a902d1b760b9c336d6494a289e070
Prince Johnson <prince.j@inapp.com>
2022-08-04 11:23:38 +0530

    profile assignment

---
+++ JavaScript/myjs.js
@@ -0,0 +1,21 @@
+// var for declaring variable
+// var myname;
+/*
+var age;
+
+//var myname = "Tom"; //use quotes for string
+var age = 15;
+// variable types
+const myname = "Const Name " // block scope, can't change value
+alert(myname)
+let myname = "Let Name"; // block scope and can be changed
+alert(myname)
+var myname = "var Name"; // global scope and function scope
+alert(myname)
+const a = 29;
+alert(a)
+*/
+// string functions and slicing
+let mytext = "Hello World amd Welcome"
+let partext = mytext.slice(4,10);
+alert(partext)
